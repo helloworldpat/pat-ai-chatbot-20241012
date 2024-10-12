144745d9b84f04dccd00492f2e8ad5243027e136
helloworldpat <dongpatthanapat@gmail.com>
2024-10-12 03:53:24 +0000

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   requirements.txt #	modified:   streamlit_app.py #

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import google.generativeai as genai
+
 st.title("🐧 My Chatbot App")
 st.subheader("Conversation")
+
+#get Gemini api key
+gemini_api_key = st.text_input("Gemini API Key: ", placeholder="Type your API Key here...", type="password")
+# genai.configure(api_key=gemini_api_key)
+# model = genai.GenerativeModel("gemini-pro")
+# response = model.generate_content(user_input)
+# bot_response = response.text
+
 
 # Initialize session state for storing chat history
 if "chat_history" not in st.session_state:
